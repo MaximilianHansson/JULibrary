b60f60ea354a53014fdac1897c25243f3c0c190e
HaMa1654 <hama1654@student.ju.se>
2018-03-27 09:50:37 +0200

    Nessesary pages created

---
+++ PresentationLayer/Controllers/AdministratorsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class AdministratorsController : Controller
+    {
+        // GET: Administrators
+        public ActionResult Index()
+        {
+            return View("Administrators");
+        }
+    }
+}
---
+++ PresentationLayer/Controllers/NewAdministratorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class NewAdministratorController : Controller
+    {
+        // GET: NewAdministrator
+        public ActionResult Index()
+        {
+            return View("newAdministrator");
+        }
+    }
+}
